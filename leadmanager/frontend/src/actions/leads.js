eacdfea2d0839859034d443e926582de236f4544
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-11-16 21:35:38 -0600

    added validation errors

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import {GET_LEADS, DELETE_LEAD, ADD_LEAD} from './types'
+import errors from '../reducers/errors';
+import {GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS} from './types'
 
 // GET Leads
 export const getLeads = () => dispatch => {
@@ -32,5 +33,14 @@ export const addLead = (lead) => dispatch => {
             payload: res.data
         })
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+        const errors = {
+            msg: err.response.data,
+            status: err.response.status 
+        };
+        dispatch({
+            type: GET_ERRORS,
+            payload: errors
+        })
+    })
 }
