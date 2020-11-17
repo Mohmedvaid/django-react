529679d5dcbd93aee240868a2f813cb31b8eaac4
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-11-16 21:59:51 -0600

    customized messages popup

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import errors from '../reducers/errors';
-import {GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS} from './types'
+import {GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS} from './types';
+import {createMessage} from './messages';
 
 // GET Leads
 export const getLeads = () => dispatch => {
@@ -17,6 +18,7 @@ export const getLeads = () => dispatch => {
 export const deleteLead = (id) => dispatch => {
     axios.delete(`/api/leads/${id}/`)
     .then(res => {
+        dispatch(createMessage({deleteLead: "Lead Deleted!"}))
         dispatch({
             type: DELETE_LEAD,
             payload: id
@@ -28,6 +30,7 @@ export const deleteLead = (id) => dispatch => {
 export const addLead = (lead) => dispatch => {
     axios.post(`/api/leads/`, lead)
     .then(res => {
+        dispatch(createMessage({addLead: "Lead Added!"}))
         dispatch({
             type: ADD_LEAD,
             payload: res.data
